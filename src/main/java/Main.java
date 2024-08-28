26e0ee7e106269f3fe6b76cc0c575ad584795263
cphmk <mkhallberg@hotmail.com>
2024-08-28 10:12:15 +0200

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import DAO.PersonDAO;
+import entities.Person;
+
 public class Main {
     public static void main(String[] args) {
 
+        PersonDAO personDAO = new PersonDAO();
+        personDAO.createPerson(Person.builder()
+                .name("Joakim")
+                .age(42)
+                .id(1)
+                .build());
     }
 }
+
+
+
